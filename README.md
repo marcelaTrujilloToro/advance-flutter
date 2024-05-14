89d416ad5a1a5a79db885089d6d6ad4a853eecd0
Marcela Trujillo Toro <marces.ttblue@gmail.com>
2024-05-14 15:32:13 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# disenos
+
+A new Flutter project.
---
+++ lib/src/widgets/headers.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SquareHeader extends StatelessWidget {
+  const SquareHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
